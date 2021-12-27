f21ba90a38b538d7cff5d405a0fb174ecf1ce904
ubunterro <finko-ilya@yandex.ru>
2021-12-27 17:12:06 +0300

    added serialization file foer unit types

---
+++ lib/src/fms_unit/model/fms_unit_types.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'fms_unit_types.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+const _$FmsUnitTypeEnumMap = {
+  FmsUnitType.fmsSubdivision: '0',
+  FmsUnitType.regionGUVD_MVD: '1',
+  FmsUnitType.regionOrCityUVD_OVD: '2',
+  FmsUnitType.policeDepartment: '3',
+};
